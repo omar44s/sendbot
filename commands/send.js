0da59af21f931ed95fecd3a15bac36e09846d2d9
omar44s <lymr90280@gmail.com>
2026-07-20 05:08:08 +0300

    Fix send modal

--- commands/send.js
+++ commands/send.js
@@ -1,14 +1,51 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder
+} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("send")
-        .setDescription("اختبار"),
+        .setDescription("إرسال رسالة"),
 
     async execute(interaction) {
-        await interaction.reply({
-            content: "البوت شغال ✅",
-            ephemeral: true
-        });
+
+        const modal = new ModalBuilder()
+            .setCustomId("send_modal")
+            .setTitle("إرسال رسالة");
+
+        const title = new TextInputBuilder()
+            .setCustomId("title")
+            .setLabel("العنوان")
+            .setStyle(TextInputStyle.Short);
+
+        const description = new TextInputBuilder()
+            .setCustomId("description")
+            .setLabel("الوصف")
+            .setStyle(TextInputStyle.Paragraph);
+
+        const color = new TextInputBuilder()
+            .setCustomId("color")
+            .setLabel("لون Embed")
+            .setRequired(false)
+            .setStyle(TextInputStyle.Short);
+
+        const banner = new TextInputBuilder()
+            .setCustomId("banner")
+            .setLabel("رابط البنر")
+            .setRequired(false)
+            .setStyle(TextInputStyle.Short);
+
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(title),
+            new ActionRowBuilder().addComponents(description),
+            new ActionRowBuilder().addComponents(color),
+            new ActionRowBuilder().addComponents(banner)
+        );
+
+        await interaction.showModal(modal);
     }
 };
